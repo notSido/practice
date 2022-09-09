7f21186b13f2ad8cb22b4a4b0ee9e688cc540977
Mikhail Sidorenko <111561235+notSido@users.noreply.github.com>
2022-09-09 08:15:01 +0200

    Update rockpaperscissors.py

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,7 +1,36 @@
 import random
 
-user_action = str(input('Rock, Paper, or Scissors?: '))
-possible_actions = ['rock','paper','scissors']
-computer_action = random.choice(possible_actions)
 
-print(computer_action)
+
+while True:
+    user_action = str(input('Rock, Paper, or Scissors?: '))
+    possible_actions = ['Rock','Paper','Scissors']
+    computer_action = random.choice(possible_actions)
+    def WinLoss():
+        if user_action == computer_action:
+            print(computer_action)
+            print('Tie!')
+        elif user_action == 'Rock' and computer_action == 'Paper':
+            print(computer_action)
+            print('You Lose!')
+        elif user_action == 'Rock' and computer_action == 'Scissors':
+            print(computer_action)
+            print('You Win!')
+        elif user_action == 'Paper' and computer_action == 'Scissors':
+            print(computer_action)
+            print('You Lose!')
+        elif user_action == 'Paper' and computer_action == 'Rock':
+            print(computer_action)
+            print('You win!')
+        elif user_action == 'Scissors' and computer_action == 'Rock':
+            print(computer_action)
+            print('You Lose!')
+        elif user_action == 'Scissors' and computer_action == 'Paper':
+            print(computer_action)
+            print('You Win!')
+        else:
+            raise Exception('Invalid Input')    
+    WinLoss()
+    decision = input('Do you want to play again? Y/N: ')
+    if decision == 'N':
+        break
